65774b5f091a73b3fc047f23b460c2cc4abc5e5a
Juan Jose Cañas <juanjo2003gg@gmail.com>
2023-06-19 13:59:21 -0500

    Solucion instrumento prototipado. Hasta el punto 3

---
+++ InstrumentoPrototipado/src/Principal/Principal.java
@@ -0,0 +1,15 @@
+package Principal;
+
+import Vistas.EventosTeclado;
+import Vistas.OcultarYVisualizar;
+import Vistas.PasarInformacion;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+//		new OcultarYVisualizar().setVisible(true);
+//		new PasarInformacion().setVisible(true);
+		new EventosTeclado().setVisible(true);
+	}
+}
